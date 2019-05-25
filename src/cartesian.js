eaa57dfec60baf3115f5f21d57fb3a21bb69009d
ehmicky <ehmicky@gmail.com>
2019-05-25 10:00:00 +0200

    Add comment

--- src/cartesian.js
+++ src/cartesian.js
@@ -35,10 +35,21 @@ const unpackParam = function({ param }) {
   return param
 }
 
-// If a parameter is a function, its return value will be used instead.
-// This can be used to generate random input for example (fuzzy testing).
-// It will be fired with all the arguments of this iteration. This allows for
-// arguments computed based on the value of other arguments.
+// If a parameter is a function, its return value will be used instead:
+//  - this can be used to generate random input for example (fuzzy testing).
+//  - it will be fired with all the arguments of this iteration. This allows
+//    for arguments computed based on the value of other arguments.
+// Closures that be used for stateful iterations:
+//  - the state will be updated on each iteration, regardless of cartesian
+//    dimensions.
+//  - because of this, this is not documented as it leads to wrong usages.
+//    Instead users can:
+//     - if state should be repeated across cartesian dimentions, either:
+//        - pre-compute iterable, or use a generator
+//        - use `info.indexes` in input function
+//     - otherwise, either:
+//        - use `info.index` in input function
+//        - use closures
 const invokeFunc = function(param, index, params) {
   if (typeof param !== 'function') {
     return param
