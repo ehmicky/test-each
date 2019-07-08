3e4d7c7caa1b04cdd5a72ed18034f700b9b9cc74
ehmicky <ehmicky@gmail.com>
2019-07-08 10:00:00 +0200

    Add a test about iterables

--- test/cartesian.js
+++ test/cartesian.js
@@ -1,5 +1,14 @@
+import test from 'ava'
+
+import testEach from '../src/main.js'
+
 import { testSnapshots } from './helpers/snapshot.js'
 
 testSnapshots('Normal iterations', [[['a']], [['a', 'b'], ['c', 'd']]])
 
 testSnapshots('No arguments', [[]])
+
+test('should return an iterable', t => {
+  const iterable = testEach(['a'])
+  t.is(typeof iterable[Symbol.iterator], 'function')
+})
