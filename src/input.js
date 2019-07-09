88540fbb13a1443f1a4a01cb995e650fb18baf32
ehmicky <ehmicky@gmail.com>
2019-07-09 10:00:00 +0200

    Add `iterable()` method

--- src/input.js
+++ src/input.js
@@ -5,13 +5,17 @@ export const parseInputs = function(inputs) {
   const inputsA = inputs.slice(0, -1)
   const callback = inputs[inputs.length - 1]
 
-  inputsA.forEach(validateInput)
+  validateInputs(inputsA)
 
   if (typeof callback !== 'function') {
     throw new TypeError('Last argument must be a function')
   }
 
   return [inputsA, callback]
+}
+
+export const validateInputs = function(inputs) {
+  inputs.forEach(validateInput)
 }
 
 const validateInput = function(input) {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { cartesianIterate } from 'fast-cartesian'
 
-import { parseInputs } from './input.js'
+import { parseInputs, validateInputs } from './input.js'
 import { addRepeat } from './repeat.js'
 import { normalizeFunc, callFuncs } from './func.js'
 import { addTitles, joinTitles } from './title.js'
@@ -9,12 +9,31 @@ import { wrapIndexes, unwrapIndexes } from './indexes.js'
 
 // Repeat a function with a combination of parameters.
 // Meant for data-driven testing and fuzzy testing.
+// Pass each combination to the final callback.
 export const each = function(...inputs) {
   const [inputsA, func] = parseInputs(inputs)
 
   const inputsB = inputsA.map(normalizeInput)
 
-  forEachLoop(inputsB, func)
+  // eslint-disable-next-line fp/no-loops
+  for (const [info, values] of forEachLoop(inputsB)) {
+    // The `title`, `titles`, etc. are passed as first argument (not the last
+    // one) so that:
+    //  - user can put `params` in an array (if needs be) using variadic
+    //    `...params`
+    //  - user can omit `params` if only the information in the first argument
+    //    is needed
+    func(info, ...values)
+  }
+}
+
+// Same but returns an iterable
+export const iterable = function*(...inputs) {
+  validateInputs(inputs)
+
+  const inputsA = inputs.map(normalizeInput)
+
+  yield* forEachLoop(inputsA)
 }
 
 const normalizeInput = function(input) {
@@ -28,7 +47,8 @@ const normalizeInput = function(input) {
 
 // We iterate over each loop instead of calculating all loops in advance
 // in order to minimize memory cost and allow huge number of combinations.
-const forEachLoop = function(inputs, func) {
+// For the same reason, `each()` does not return anything.
+const forEachLoop = function*(inputs) {
   // eslint-disable-next-line fp/no-let
   let index = -1
 
@@ -37,20 +57,9 @@ const forEachLoop = function(inputs, func) {
     // eslint-disable-next-line fp/no-mutation
     index += 1
 
-    handleLoop(loop, index, func)
+    const { values, ...info } = normalizeLoop(loop, index)
+    yield [info, values]
   }
-}
-
-const handleLoop = function(loop, index, func) {
-  const { values, ...loopA } = normalizeLoop(loop, index)
-
-  // The `title`, `titles`, etc. are passed as first argument (not the last one)
-  // so that:
-  //  - user can put `params` in an array (if needs be) using variadic
-  //    `...params`
-  //  - user can omit `params` if only the information in the first argument is
-  //    needed
-  func(loopA, ...values)
 }
 
 const normalizeLoop = function(loop, index) {
